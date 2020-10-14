c0500351c559c5652443e870bb3e0d336d8ec775
Praneeth <18praneeth@gmail.com>
2020-10-14 17:40:45 +0530

    Added Gallery

--- static_in_env/assets/js/main.js
+++ static_in_env/assets/js/main.js
@@ -148,6 +148,28 @@
     }
   });
 
+    // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    }
+  });
+
   // Init AOS
   $(window).on('load', function() {
     AOS.init({

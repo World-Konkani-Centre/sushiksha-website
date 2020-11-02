7c3f737a353f5463ed952f2c1fdf654494753d4d
Ubuntu <ubuntu@ip-172-31-35-183.ap-south-1.compute.internal>
2020-11-02 16:25:47 +0000

    updated settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4t5a^fxy%ntiqxx10xq&6)+rk^_q2*o#nuuoh6#$6j^xp7#w@_'
-
+SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['15.206.157.116', 'sushiksha.konkanischolarship.com']
 # Application definition
@@ -138,8 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sophists.bot@gmail.com'
-EMAIL_HOST_PASSWORD = 'sophists.bot@123'
+EMAIL_HOST_USER = config['EMAIL_USER']
+EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
 
 # TinyMCE
 TINYMCE_DEFAULT_CONFIG = {

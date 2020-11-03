6a83b5926ca279562b755e4228629ac7d74e2657
Praneeth <18praneeth@gmail.com>
2020-11-03 12:48:00 +0530

    Updated settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import json
 
-with open('/etc/config.json') as config_file:
-    config = json.load(config_file)
+# import json
+
+
+#Used in Production
+# with open('/etc/config.json') as config_file:
+#     config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = '2Cv8gKl3sv572JnCmZFAkyJVpF4'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -141,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config['EMAIL_USER']
-EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'EMAIL_USER'
+EMAIL_HOST_PASSWORD = 'EMAIL_PASS'
 
 # TinyMCE
 TINYMCE_DEFAULT_CONFIG = {

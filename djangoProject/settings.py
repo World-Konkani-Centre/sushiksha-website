0d57d5c8d3dd53016bbda2023fae0ce2ae56c131
Praneeth <18praneeth@gmail.com>
2020-11-08 22:05:22 +0530

    updated config.json

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import json
 
-with open('/etc/config.json') as config_file:
+with open('../configs/config.json') as config_file:
     config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

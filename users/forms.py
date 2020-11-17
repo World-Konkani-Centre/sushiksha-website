61f205289b06c61978c3d5c0951b9410e8856a72
Praneeth <18praneeth@gmail.com>
2020-11-17 13:47:31 +0530

    Tried adding a searchable drop down menu for badge system

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile, Reward
 
+#
+# def get_user_list(user):
+#     CHOICE = []
+#     for i in user.objects.all():
+#         print(type(i))
+#         user_set = (i.username, i)
+#         CHOICE.append(user_set)
+#     return CHOICE
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -44,3 +53,21 @@ class RewardForm(forms.ModelForm):
     class Meta:
         model = Reward
         fields = ['description', 'badges']
+
+
+class BadgeForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'placeholder': 'Describe why your are giving the badge',
+        'rows': 2
+    }))
+
+    class Meta:
+        model = Reward
+        fields = ['user', 'description', 'badges']
+
+    def __init__(self, *args, **kwargs):
+        super(BadgeForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            print(visible.form)
+            visible.field.widget.attrs['class'] = 'fstdropdown-select'

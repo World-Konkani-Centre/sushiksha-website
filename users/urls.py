cf17d869f115fdfc69a4c7685543a03c184bfda5
Praneeth <18praneeth@gmail.com>
2020-12-18 22:40:54 +0530

    Badge list

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from badge.views import badge_list
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
@@ -12,6 +13,6 @@ urlpatterns = [
     path('search/', views.search, name='user-search'),
     path('badge/<int:id>/', views.create_badge, name='new-badge'),
     path('badge/', views.badge, name='badge'),
-    # path('rewards/', RewardListView.as_view(), name='reward'),
+    path('rewards/', badge_list, name='reward'),
     path('<int:pk>/', views.user_detail_view, name='user-detail'),
 ]

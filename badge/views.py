464a314b7fd69acd2f2c40e1f58c68e28b0a800d
Praneeth <18praneeth@gmail.com>
2021-01-15 22:45:16 +0530

    Filtering and pagination

--- badge/views.py
+++ badge/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from users.models import Reward
 from django.db.models import Q
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from .filters import RewardFilter
 
 
 def badge_list(request):
     query = Reward.objects.order_by('-timestamp')
-    paginator = Paginator(query, 30)
+
+    f = RewardFilter(request.GET, queryset=query)
+    paginated_queryset = f.qs
+
+    paginator = Paginator(paginated_queryset, 10)
 
     page_request_var = 'page'
     page = request.GET.get(page_request_var)
@@ -19,6 +24,7 @@ def badge_list(request):
 
     context = {
         'query': paginated_queryset,
+        'reward_filter': f,
         'page_request_var': page_request_var,
         'title': "Badges awarded"
     }

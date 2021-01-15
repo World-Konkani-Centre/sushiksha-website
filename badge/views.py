ba016e326588b218e020428e5f2d585a06b5ecca
Praneeth <18praneeth@gmail.com>
2021-01-15 23:48:03 +0530

    Urls.py updated with claim forms

--- badge/views.py
+++ badge/views.py
@@ -31,32 +31,5 @@ def badge_list(request):
     return render(request, 'rewards.html', context=context)
 
 
-def badge_search(request):
-    queryset = Reward.objects.order_by('-timestamp')
-    query = request.GET.get('q')
-    if query:
-        queryset = queryset.filter(
-            Q(user__username__icontains=query) |
-            Q(user__profile__name__icontains=query) |
-            Q(awarded_by=query) |
-            Q(badges__title__icontains=query)
-        ).distinct()
-
-    paginator = Paginator(queryset, 40)
-
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
-    try:
-        paginated_queryset = paginator.page(page)
-    except PageNotAnInteger:
-        paginated_queryset = paginator.page(1)
-    except EmptyPage:
-        paginated_queryset = paginator.page(paginator.num_pages)
-
-    context = {
-        'query': paginated_queryset,
-        'page_request_var': page_request_var,
-        'title': "Badges awarded"
-    }
-
-    return render(request, 'rewards.html', context=context)
+def donut_form(request):
+    return render(request, 'badge_claim/donut.html')

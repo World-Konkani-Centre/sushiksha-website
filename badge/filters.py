c02030cd3835eb6a54b75353345b01784e276f23
swasthik <swasthik0304@gmail.com>
2021-01-19 21:58:40 +0530

    ordering the names in badge list filter.

--- badge/filters.py
+++ badge/filters.py
@@ -1,10 +1,11 @@
 import django_filters
-from users.models import Reward
-from django_filters import CharFilter
+from users.models import Reward, User
+from django_filters import CharFilter, ModelChoiceFilter
 
 
 class RewardFilter(django_filters.FilterSet):
     awarded_by = CharFilter(field_name='awarded_by', lookup_expr='icontains')
+    user = ModelChoiceFilter(queryset=User.objects.all().order_by('username'))
 
     class Meta:
         model = Reward

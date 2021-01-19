e9cc0aeb64cad066f18d3ae2a1bcf189a75b710c
swasthik <swasthik0304@gmail.com>
2021-01-19 22:15:41 +0530

    case insensitive comparision

--- badge/filters.py
+++ badge/filters.py
@@ -1,11 +1,12 @@
 import django_filters
 from users.models import Reward, User
 from django_filters import CharFilter, ModelChoiceFilter
+from django.db.models.functions import Lower
 
 
 class RewardFilter(django_filters.FilterSet):
     awarded_by = CharFilter(field_name='awarded_by', lookup_expr='icontains')
-    user = ModelChoiceFilter(queryset=User.objects.all().order_by('username'))
+    user = ModelChoiceFilter(queryset=User.objects.all().order_by(Lower('username')))
 
     class Meta:
         model = Reward

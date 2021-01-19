6e994e92bd11c61aa800c18cda073718225a22bf
Praneeth <18praneeth@gmail.com>
2021-01-19 22:13:56 +0530

    Ordering of user

--- badge/filters.py
+++ badge/filters.py
@@ -1,10 +1,13 @@
 import django_filters
 from users.models import Reward
-from django_filters import CharFilter
+from django.contrib.auth.models import User
+from django_filters import CharFilter, ModelChoiceFilter
+from django.db.models.functions import Lower
 
 
 class RewardFilter(django_filters.FilterSet):
     awarded_by = CharFilter(field_name='awarded_by', lookup_expr='icontains')
+    user = ModelChoiceFilter(queryset=User.objects.all().order_by(Lower('username')))
 
     class Meta:
         model = Reward

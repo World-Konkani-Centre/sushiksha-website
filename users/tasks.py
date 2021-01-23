a076532c214e0abf9d71288752ee4ae915182d89
swasthik <swasthik0304@gmail.com>
2021-01-23 23:44:25 +0530

    single background task runner, updating the configurations of the celery. using proper log messages.

--- users/tasks.py
+++ users/tasks.py
@@ -9,10 +9,5 @@ logger = get_task_logger(__name__)
 @shared_task
 def send_email(array):
     send_reward_mail(array)
-    return True
-
-
-@shared_task
-def send_in_slack(array):
     send_reward_slack(array)
-    return True
+    return "\n".join(array)+"\nTrue"

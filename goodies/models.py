57b6cc984e98314c9761f4ea6e853af1a7ba5a33
swasthik <swasthik0304@gmail.com>
2021-02-01 14:59:31 +0530

    updating the goodie display

--- goodies/models.py
+++ goodies/models.py
@@ -13,11 +13,10 @@ class Goodie(models.Model):
     title = models.CharField(max_length=30)
     tag = models.CharField(max_length=10)
     is_shown = models.BooleanField(default=True)
-    thumbline = models.ImageField(upload_to='goodies')
+    thumbnail = models.ImageField(upload_to='goodies')
     is_link = models.BooleanField(default=False)
     description = models.TextField()
     is_image = models.BooleanField(default=True)
-    hd_image = models.ImageField(upload_to='goodies', blank=True, null=True)
     link = models.URLField(blank=True, null=True)
 
     def __str__(self):

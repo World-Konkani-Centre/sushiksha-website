24a683de18ebe7d4c13151fd06d98d18b5377dec
Praneeth <18praneeth@gmail.com>
2021-02-09 15:45:31 +0530

    Registering the claim forms

--- badge/models.py
+++ badge/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class BadgeClaim(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
+    
+

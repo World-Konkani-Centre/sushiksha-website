5279a38d7ecb308ece709c848f9f4e3cdb4a5fec
swasthik <swasthik0304@gmail.com>
2021-02-12 17:33:49 +0530

    admin access change for okr

--- OKR/admin.py
+++ OKR/admin.py
@@ -1,17 +1,17 @@
-from django.contrib import admin
-from .models import Objective, KR, Entry
-
-
-@admin.register(Objective)
-class ObjectiveAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(KR)
-class KRAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Entry)
-class EntryAdmin(admin.ModelAdmin):
-    pass
+# from django.contrib import admin
+# from .models import Objective, KR, Entry
+#
+#
+# @admin.register(Objective)
+# class ObjectiveAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(KR)
+# class KRAdmin(admin.ModelAdmin):
+#     pass
+#
+#
+# @admin.register(Entry)
+# class EntryAdmin(admin.ModelAdmin):
+#     pass

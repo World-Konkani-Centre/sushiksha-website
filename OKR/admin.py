b7c3d6750b938daf3fd2643a4ef60601554d419a
swasthik <swasthik0304@gmail.com>
2021-02-12 18:30:09 +0530

    admin panel corrected

--- OKR/admin.py
+++ OKR/admin.py
@@ -1,17 +1,24 @@
-# from django.contrib import admin
-# from .models import Objective, KR, Entry
-#
-#
-# @admin.register(Objective)
-# class ObjectiveAdmin(admin.ModelAdmin):
-#     pass
-#
-#
-# @admin.register(KR)
-# class KRAdmin(admin.ModelAdmin):
-#     pass
-#
-#
+from django.contrib import admin
+from .models import Objective, KR, Entry
+from django import forms
+
+
+@admin.register(Objective)
+class ObjectiveAdmin(admin.ModelAdmin):
+    list_display = ('user', 'objective')
+    list_filter = ('user',)
+    search_fields = ('user', 'objective', 'objective__key_result')
+
+
+@admin.register(KR)
+class KRAdmin(admin.ModelAdmin):
+    list_display = ( 'user','objective','key_result')
+    list_filter = ('objective__user__profile__name', 'objective')
+    search_fields = ('objective__user', 'objective', 'key_result')
+
+    def user(self,obj):
+        return obj.objective.user.profile.name
+    user.admin_order_field = 'user'
 # @admin.register(Entry)
 # class EntryAdmin(admin.ModelAdmin):
 #     pass

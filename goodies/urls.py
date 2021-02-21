e465fd5ba2bc3b767c230f183ec6e73ec5dee248
swasthik <swasthik0304@gmail.com>
2021-02-21 17:52:20 +0530

    goodie section claim complete

--- goodies/urls.py
+++ goodies/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.goodies, name='goodies'),
     path('cart/<int:id>', views.cart, name='cart'),
+    path('place/<int:id>',views.place,name='place-goodie'),
     path('tag/<str:tag>', views.category_goodies, name='tag-goodies'),
 ]
--- goodies/views.py
+++ goodies/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Goodie
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
+
+from users.models import Profile
+from .models import Goodie, Orders
 
 
 def goodies(request):
@@ -20,10 +24,25 @@ def cart(request, id):
     return render(request, 'goodies/cart.html', context)
 
 
-def category_goodies(request,tag):
-    queryset = Goodie.objects.filter(is_shown=True,tag=tag)
+def category_goodies(request, tag):
+    queryset = Goodie.objects.filter(is_shown=True, tag=tag)
     context = {
-        'queryset':queryset,
-        'title':'Goodies'
+        'queryset': queryset,
+        'title': 'Goodies'
     }
     return render(request, 'goodies/goodies.html', context)
+
+
+def place(request, id):
+    goodie = get_object_or_404(Goodie, id=id)
+    user = get_object_or_404(User, id=request.user.id)
+    profile = get_object_or_404(Profile, user=user)
+    if profile.suShells < goodie.suShells or profile.rank != "Caesar":
+        messages.error(request, "Your order for " + goodie.title + " could not be placed, insufficient amount/rank not "
+                                                                   "attained")
+        return redirect('goodies')
+    profile.suShells -= goodie.suShells
+    profile.save()
+    Orders.objects.create(user=user, goodie=goodie)
+    messages.success(request, "Your order for " + goodie.title + " has been placed")
+    return redirect('goodies')
